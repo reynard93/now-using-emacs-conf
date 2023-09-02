660c7a765985ccc58046fd2d041ae410da0d9e66
Reynard <reynard_lee_from.tp@mom.gov.sg>
2023-09-02 15:43:37 +0800

    Add mu4e feature

--- modules/mail.el
+++ modules/mail.el
@@ -5,12 +5,13 @@
 ;;   xdg-settings set default-url-scheme-handler mailto firefox.desktop
 ;; then set gmail website as default mailto associated application in firefox in 'setting'
 ;;; Code:
-
 (defun mk/setup-mail()
   "Setup Email Configuration."
   ;; (setq message-send-mail-function 'message-send-mail-with-mailclient)
   (setq send-mail-function 'sendmail-send-it
-    sendmail-program "/usr/bin/msmtp"
+    message-send-mail-function 'sendmail-send-it
+    sendmail-program (executable-find "msmtp")
+    message-sendmail-envelope-from 'header
     ;; mail-specify-envelope-from t
     ;; message-sendmail-envelope-from 'header
     message-auto-save-directory "~/mail_save"
@@ -18,35 +19,59 @@
 
 (add-hook 'after-init-hook #'mk/setup-mail)
 
-(use-package mu4e
-  ;; installing 'mu' though 'pacman -S mu' will automatically add package 'mu4e' into emacs site-package
-  :straight nil
-  :ensure nil
-  :config
-  (setq mu4e-mu-binary (executable-find "mu")
-    mu4e-confirm-quit nil
-    mu4e-maildir-list '("~/mail")
-    mu4e-get-mail-command "offlineimap"
-    mu4e-update-interval 300
-    ;; mu4e-sent-messages-behavior 'delete ;; It seems like the imap server for 'mr.meowking@anche.no' won't backup mails sent by clients. So the sent mail should be stored in our local machine.
-    mu4e-user-mail-address-list '("mr.meowking@anche.no")
-    mu4e-use-fancy-chars t)
-  
-  (setq mu4e-sent-folder "/autistici/Sent"
-    mu4e-drafts-folder "/autistici/Drafts"
-    mu4e-trash-folder "/autistici/Trash")
-
-  (setq mu4e-maildir-shortcuts
-    '(("/autistici/INBOX" . ?i))))
-
 (defun mk/mu4e-main-local-keybinding-setup()
   (keymap-local-set "J" #'mu4e-search-maildir))
-
 (defun mk/mu4e-view-local-keybinding-setup()
   ;; SPC-m r
   (keymap-local-set "M-r" #'mu4e-compose-reply))
 
-(add-hook 'mu4e-main-mode-hook 'mk/mu4e-main-local-keybinding-setup)
-(add-hook 'mu4e-view-mode-hook 'mk/mu4e-view-local-keybinding-setup)
+(use-package mu4e
+  :load-path  "/opt/homebrew/share/emacs/site-lisp/mu/mu4e"
+  ;; installing 'mu' though 'pacman -S mu' will automatically add package 'mu4e' into emacs site-package
+  :straight nil
+  :ensure nil
+  :demand t
+  :hook ((mu4e-main-mode . mk/mu4e-main-local-keybinding-setup)
+          (mu4e-view-mode . mk/mu4e-view-local-keybinding-setup))
+  :config
+  (setq
+    mu4e-mu-binary (executable-find "mu")
+    mu4e-confirm-quit nil
+    mu4e-maildir "~/.maildir"
+    ;; Lists frequently used mail directories for quick access.
+    mu4e-maildir-list '("~/mail")
+    mu4e-get-mail-command (concat (executable-find "mbsync") " -a")
+    mu4e-update-interval 300
+    mu4e-attachment-dir "~/Desktop"
+    ;; rename files when moving - needed for mbsync:
+    mu4e-change-filenames-when-moving t
+    mu4e-user-mail-address-list '("yylee.rey@gmail.com")
+    mu4e-use-fancy-chars t
+    ;; setup shortcut with 'i' path is also relative to mu4e-maildir.
+    mu4e-maildir-shortcuts
+    '(("/gmail/INBOX" . ?g)
+       ("/gmail/[Gmail]/Sent Mail" . ?G))
+    mu4e-contexts
+    `(,(make-mu4e-context
+         :name "gmail"
+         :enter-func
+         (lambda () (mu4e-message "Enter yylee.rey@gmail.com context"))
+         :leave-func
+         (lambda () (mu4e-message "Leave yylee.rey@gmail.com context"))
+         :match-func
+         (lambda (msg)
+           (when msg
+             (mu4e-message-contact-field-matches msg
+               :to "dummy@gmail.com")))
+         :vars '((user-mail-address . "yylee.rey@gmail.com")
+                  (user-full-name . "Reynard Lee")
+                  (mu4e-drafts-folder . "/gmail/Drafts")
+                  (mu4e-refile-folder . "/gmail/Archive")
+                  (mu4e-sent-folder . "/gmail/Sent")
+                  (mu4e-trash-folder . "/gmail/Trash"))))
+    mu4e-context-policy 'pick-first ;; start with the first (default) context;
+    mu4e-compose-context-policy 'ask))
 
+;; for sending mails
+(require 'smtpmail)
 (provide 'mail)
