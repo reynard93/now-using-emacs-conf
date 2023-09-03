fda795c1be417146ebb5daaea732acc82100a213
Reynard <reynard_lee_from.tp@mom.gov.sg>
2023-09-03 16:01:33 +0800

    remove eyebrowse

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -128,18 +128,18 @@ Version 2016-04-04"
 ;; TODO try tabspace 
 
 ;; @ switch workspace
-(use-package eyebrowse
-  :config 
-	(define-key eyebrowse-mode-map (kbd "M-0") 'eyebrowse-switch-to-window-config-0)
-	(define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
-	(define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
-	(define-key eyebrowse-mode-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
-	(define-key eyebrowse-mode-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
-	(define-key eyebrowse-mode-map (kbd "M-5") 'eyebrowse-switch-to-window-config-5)
-	(eyebrowse-mode t)
-	(setq eyebrowse-wrap-around t) ;; makes workspaces a loop
-	;; (setq eyebrowse-new-workspace "*dashboard*")
-  ) ;; use *scratch* buffer (use string to provide it with custom buffer name)
+;; (use-package eyebrowse
+;;   :config 
+;; 	(define-key eyebrowse-mode-map (kbd "M-0") 'eyebrowse-switch-to-window-config-0)
+;; 	(define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+;; 	(define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+;; 	(define-key eyebrowse-mode-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+;; 	(define-key eyebrowse-mode-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
+;; 	(define-key eyebrowse-mode-map (kbd "M-5") 'eyebrowse-switch-to-window-config-5)
+;; 	(eyebrowse-mode t)
+;; 	(setq eyebrowse-wrap-around t) ;; makes workspaces a loop
+;; 	;; (setq eyebrowse-new-workspace "*dashboard*")
+;;   ) ;; use *scratch* buffer (use string to provide it with custom buffer name)
 
 ;; (use-package tabspaces
 ;;   ;; use this next line only if you also use straight, otherwise ignore it. 
